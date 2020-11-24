aa404a693eb05ab1c550630db21d757d77eae2fb
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2020-11-23 17:13:57 -0800

    Add prop type validation

--- src/components/Kingdom/KingdomDepartments/KingdomDepartments.js
+++ src/components/Kingdom/KingdomDepartments/KingdomDepartments.js
@@ -1,28 +1,36 @@
-import React from 'react';
-import './KingdomDepartments.css'
+import React, {Component} from 'react';
+import './KingdomDepartments.css';
+import PropTypes from "prop-types";
 
-const kingdomDepartments = (props) => {
-  let level = null;
-
-  switch (props.types) {
-    case ("defence"):
-      level = <div className={"Defence"}></div>;
-      break;
-    case ("science"):
-      level = (
-        <div className={"Science"}>;
-          <div className={"Seeds1"}></div>;
-          <div className={"Seeds2"}></div>;
-        </div>
-      )
-      break;
-    case ("construction"):
-      level = <div className={"Construction"}></div>;
-      break;
-    case ("social-services"):
-      level = <div className={"Social-Services"}></div>
+class KingdomDepartments extends Component {
+  render () {
+    let level = null;
+    switch (this.props.type) {
+      case ("defence"):
+        level = <div className={"Defence"}></div>;
+        break;
+      case ("science"):
+        level = (
+          <div className={"Science"}>;
+            <div className={"Seeds1"}></div>;
+            <div className={"Seeds2"}></div>;
+          </div>
+        )
+        break;
+      case ("construction"):
+        level = <div className={"Construction"}></div>;
+        break;
+      case ("social-services"):
+        level = <div className={"Social-Services"}></div>;
+        break;
+      default:
+        level = null;
+    }
+    return level;
   }
-  return ();
 }
 
-export default kingdomDepartments;
+KingdomDepartments.propTypes = {
+  type: PropTypes.string.isRequired
+}
+export default KingdomDepartments;
